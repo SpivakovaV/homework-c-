1c5304154bc71659584107bff8c1a8079e36f3ed
Vita <spivakovav@mail.ru>
2022-12-07 16:56:09 +0300

    191122 task15

---
+++ homework_1911/task15/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным. 6 -> да 7 -> да 1 -> нет
+
+using static System.Console;
+Clear();
+
+bool HolidayChek(int day) { return day>5 ? true : false; }
+int day;
+do
+{
+    Write("Введите порядковый номер дня недели: ");
+    day = Convert.ToInt32(ReadLine());
+} while (day > 7||day<1);
+
+if (HolidayChek(day)) Write("Ура! Этот день выходной.");
+else Write("Этот день не является выходным.");
