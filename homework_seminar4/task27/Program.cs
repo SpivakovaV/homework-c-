b13f643993d904d1e1b1e67d13b9d9c25f2306c5
Vita <spivakovav@mail.ru>
2022-12-09 18:47:32 +0300

    task27

--- homework_seminar4/task27/Program.cs
+++ homework_seminar4/task27/Program.cs
@@ -1,1 +1,20 @@
 ﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+using static System.Console;
+Clear();
+
+int sum(int digit)
+{
+    int result = 0;
+    while (digit != 0)
+    {
+        result += digit%10;
+        digit /= 10;
+    }
+    return result;
+}
+
+Write("Введите число: ");
+WriteLine($"Сумма цифр в числе: {sum(int.Parse(ReadLine()))}");
+
