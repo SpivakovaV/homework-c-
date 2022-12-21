2d05d86d6d91993662de8de7ab6a90f180f919b9
Vita <spivakovav@mail.ru>
2022-12-21 16:38:07 +0300

    task58

--- homework_seminar8/task58/Program.cs
+++ homework_seminar8/task58/Program.cs
@@ -3,71 +3,68 @@
 using static System.Console;
 Clear();
 
-WriteLine("\nEnter the sizes of the matrices and the range of random values: ");
-int m = InputNumbers("Enter the number of rows of the 1st matrix: ");
-int n = InputNumbers("Enter the number of columns of the 1st and rows: ");
-int s = InputNumbers("Enter the numbers of rows of the 2nd matrix: ");
-int p = InputNumbers("Enter the number of columns of the 2nd matrix: ");
-int range = InputNumbers("Enter a range of random numbers: from 0 to ");
-
-int[,] firstMartrix = new int[m, n];
-CreateArray(firstMartrix);
-WriteLine($"\nFirst matrix:");
-WriteArray(firstMartrix);
-
-int[,] secomdMartrix = new int[s, p];
-CreateArray(secomdMartrix);
-WriteLine($"\nSecond matrix:");
-WriteArray(secomdMartrix);
-
-int[,] resultMatrix = new int[m, p];
-
-MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
-WriteLine($"\nMultiplication of the first and the second matrices:");
-WriteArray(resultMatrix);
-
-void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
-{
-    for (int i = 0; i < resultMatrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+void Print(int[,] a) //Метод выводит заданную матрицу в консоль.
         {
-            int sum = 0;
-            for (int k = 0; k < firstMartrix.GetLength(1); k++)
+            for (int i = 0; i < a.GetLength(0); i++)
             {
-                sum += firstMartrix[i, k] * secomdMartrix[k, j];
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    Write("{0} ", a[i, j]);
+                }
+                WriteLine();
             }
-            resultMatrix[i, j] = sum;
-        }
-    }
-}
-
-int InputNumbers(string input)
-{
-    Write(input);
-    int output = Convert.ToInt32(ReadLine());
-    return output;
-}
-
-void CreateArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
+        }        
+int[,] MatrixMultiplication(int[,] a, int[,] b) // Метод перемножения матриц.
         {
-            array[i, j] = new Random().Next(range);
+            int[,] r = new int[a.GetLength(0), b.GetLength(1)];
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < b.GetLength(1); j++)
+                {
+                    for (int k = 0; k < b.GetLength(0); k++)
+                    {
+                        r[i,j] += a[i,k] * b[k,j];
+                    }
+                }
+            }
+            return r;
         }
-    }
-}
 
-void WriteArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Write(array[i, j] + " ");
-        }
-        WriteLine();
-    }
-}
+WriteLine("Введите размерность первой матрицы: ");
+            int[,] A = new int[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < A.GetLength(1); j++)
+                {
+                    Console.Write("A[{0},{1}] = ", i, j);
+                    A[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+WriteLine("Введите размерность второй матрицы: ");
+            int[,] B = new int[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
+            for (int i = 0; i < B.GetLength(0); i++)
+            {
+                for (int j = 0; j < B.GetLength(1); j++)
+                {
+                    Console.Write("B[{0},{1}] = ", i, j);
+                    B[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+WriteLine("Матрица A:");
+Print(A);
+WriteLine();
+
+WriteLine("Матрица B:");
+Print(B);
+WriteLine();
+            
+if (A.GetLength(1) != B.GetLength(0)) Write("Матрицы нельзя перемножить!");
+   else { 
+         WriteLine("Результат перемножения матриц: ");
+         int[,] C = MatrixMultiplication(A, B);
+         Print(C);
+         }
+
+WriteLine();
