5ba9524bac1d89b04aa4f462b66c109d741a1a04
Vita <spivakovav@mail.ru>
2022-12-21 14:55:11 +0300

    task54

---
+++ homework_seminar8/task54/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+
+using static System.Console;
+Clear();
+
+void FillArray(int[,]array) //Метод создания массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Random x = new Random();
+            array[i, j] = x.Next(10);
+        }
+    }
+}
+
+void PrintArray(int[,] array) //Метод вывода массива в консоль
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+void ArrayRowsDescendingOrder(int[,] array) //Метод обработки двумерного массива на упорядочивание позиций в строке по убыванию.
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+Write("Сколько строк будет в массиве: ");
+int m=int.Parse(ReadLine());
+Write("Сколько столбцов будет в массиве: ");
+int n = int.Parse(ReadLine());
+
+int[,]array=new int[m,n];
+
+FillArray(array);
+
+WriteLine("Создан массив: ");
+PrintArray(array);
+WriteLine();
+
+ArrayRowsDescendingOrder(array);
+
+WriteLine("Массив упорядочен: ");
+PrintArray(array);
+WriteLine();
---
+++ homework_seminar8/task58/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+using static System.Console;
+Clear();
+
+WriteLine("\nEnter the sizes of the matrices and the range of random values: ");
+int m = InputNumbers("Enter the number of rows of the 1st matrix: ");
+int n = InputNumbers("Enter the number of columns of the 1st and rows: ");
+int s = InputNumbers("Enter the numbers of rows of the 2nd matrix: ");
+int p = InputNumbers("Enter the number of columns of the 2nd matrix: ");
+int range = InputNumbers("Enter a range of random numbers: from 0 to ");
+
+int[,] firstMartrix = new int[m, n];
+CreateArray(firstMartrix);
+WriteLine($"\nFirst matrix:");
+WriteArray(firstMartrix);
+
+int[,] secomdMartrix = new int[s, p];
+CreateArray(secomdMartrix);
+WriteLine($"\nSecond matrix:");
+WriteArray(secomdMartrix);
+
+int[,] resultMatrix = new int[m, p];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+WriteLine($"\nMultiplication of the first and the second matrices:");
+WriteArray(resultMatrix);
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < firstMartrix.GetLength(1); k++)
+            {
+                sum += firstMartrix[i, k] * secomdMartrix[k, j];
+            }
+            resultMatrix[i, j] = sum;
+        }
+    }
+}
+
+int InputNumbers(string input)
+{
+    Write(input);
+    int output = Convert.ToInt32(ReadLine());
+    return output;
+}
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(range);
+        }
+    }
+}
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write(array[i, j] + " ");
+        }
+        WriteLine();
+    }
+}
